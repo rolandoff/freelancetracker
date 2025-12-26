651141b8bedb647f381d91e5d1d91ab724b4b84f
Rolando Fernandez <rfernandez@pros.com>
2025-12-26 05:25:38 +0100

    fix: correct client relationship name in projects query
    - Change select from 'clients(*)' to 'client:clients(*)'
    - This allows accessing project.client.name in the table
    - Fixes client column showing '-' instead of client name

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -78,7 +78,7 @@ export function Projects() {
 
       let query = supabase
         .from('projects')
-        .select('*, clients(*)')
+        .select('*, client:clients(*)')
         .eq('user_id', user.id)
 
       if (!showArchived) {
